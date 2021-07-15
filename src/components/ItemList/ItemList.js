03961a0496b693ec38ffb4cdaf7542d5f83b7e90
JonatS321 <info.soluciones.js@gmail.com>
2021-07-15 20:45:41 -0300

    CORRECIONES ENTREGA FIREBASE

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,31 +2,49 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Item from "../Item/Item";
 import axios from "axios";
+import { db } from "../../firebase";
 import "./ItemList.css";
 
 const ItemList = ({ category = "" }) => {
     const [items, setItems] = useState([]);
+    const [products, setProducts] = useState([]);
+
     let filtered_items = [];
 
-    useEffect(() => {
-        const promesa = new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve(
-                    axios("https://demo2045556.mockable.io/productos").then(
-                        (res) => setItems(res.data)
-                    )
-                );
-            }, 2);
+    const getItems = () =>
+        db.collection("products").onSnapshot((doc) => {
+            const items = [];
+            doc.forEach((docu) => {
+                items.push({ ...docu.data(), id: docu.id });
+                console.log(docu.data(), docu.id);
+            });
+            console.log(items);
+            setItems(items);
         });
+
+    // No me dejo almacenar "setItems(filtered_items)"
+    //por eso lo hice de esta manera.
+    if (category != "") {
+        filtered_items = items.filter((item) => item.category == category);
+    } else {
+        filtered_items = items;
+    }
+
+    useEffect(() => {
+        getItems();
     }, []);
 
-    if ( category  != "") {
-        filtered_items = items.filter((item) => item.category ==  category );
-    }
-    else {filtered_items = items}
-
-// No me dejo almacenar "setItems(filtered_items)"
-//por eso lo hice de esta manera.
+    //    useEffect(() => {
+    //        const promesa = new Promise((resolve, reject) => {
+    //            setTimeout(() => {
+    //                resolve(
+    //                    axios("https://demo2045556.mockable.io/productos").then(
+    //                        (res) => setItems(res.data)
+    //                    )
+    //                );
+    //            }, 2);
+    //        });
+    //    }, []);
 
     return (
         <div className="ItemList">
@@ -40,23 +58,3 @@ const ItemList = ({ category = "" }) => {
 };
 
 export default ItemList;
-
-//return (
-//    <div className="ItemList">
-//        {items.map((producto) => (
-//            <Link className="link" to= {`/item/${producto.id}`} >
-//                <Item key={producto.id} itemArray={producto} />
-//            </Link>
-//        ))}
-//    </div>
-//);
-
-//return (
-//    <div className="ItemList">
-//        {(items.filter(item => item.category == category)).map((producto) => (
-//            <Link className="link" to={`/item/${producto.id}`}>
-//                <Item key={producto.id} itemArray={producto} />
-//            </Link>
-//        ))}
-//    </div>
-//);
--- src/views/ItemDetailContainer/ItemDetailContainer.js
+++ src/views/ItemDetailContainer/ItemDetailContainer.js
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from "react";
 import ItemDetail from "../../components/ItemDetail/ItemDetail";
+import { db } from "../../firebase";
 
 //No encontre la manera de que me devolviera un solo producto
 //usando lo visto en clase, debo haber creado mal el API
 
-const ItemDetailContainer = ({match}) => {
+const ItemDetailContainer = ({ match }) => {
     let id = match.params.id;
     const [item, setItem] = useState([]);
 
+    const getItem = () => {
+        db.collection("products").onSnapshot((querySnapshot) => {
+            const products = [];
+            querySnapshot.forEach((item) => {
+                if (item.id === id) {
+                    setItem(item.data());
+                }
+            });
+        });
+    };
+    
+    //    useEffect(() => {
+    //        setTimeout(() => {
+    //            fetch(`https://demo2045556.mockable.io/productos`)
+    //            .then((response) => response.json())
+    //            .then((data) => setItem(data[id-1]));
+    //        }, 20);
+    //    }, []);
+
     useEffect(() => {
-        setTimeout(() => {
-            fetch(`https://demo2045556.mockable.io/productos`)
-            .then((response) => response.json())
-            .then((data) => setItem(data[id-1]));
-        }, 20);
-    }, []);
+        getItem();
+    });
 
     return (
         <div>
-            <ItemDetail arrayProduct={item}/>
+            <ItemDetail arrayProduct={item} />
         </div>
-    )
+    );
 };
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
